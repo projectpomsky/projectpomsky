729e17efe4f8543dffcf4e422cceb50d98d05ed4
light <light@lights-MacBook-Pro.local>
2022-10-13 18:21:27 -0600

    progress on random show/hide complaint logic

--- index.js
+++ index.js
@@ -8,31 +8,26 @@ import axios from 'axios';
 const chance = require('chance').Chance();
 const functions = require('@google-cloud/functions-framework');
 
+// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// dotenv.config()
+
 const auth_header = `token ${process.env.GITHUB_TOKEN}`;
 
 // Register a CloudEvent callback with the Functions Framework that will
-// be executed when the Pub/Sub trigger topic receives a message.
-functions.cloudEvent('helloPubSub', cloudEvent => {
-  // The Pub/Sub message is passed as the CloudEvent's data payload.
-  const base64name = cloudEvent.data.message.data;
+// be executed when the Pub/Sub trigger topic receives a message from the Cloud Scheduler cron job.
+functions.cloudEvent('helloPubSub', async cloudEvent => {
 
-  const name = base64name
-    ? Buffer.from(base64name, 'base64').toString()
-    : 'World';
-
-  console.log(`Hello, ${name}!`);
-
-  // on a regular interval when this is invoked, this will either randomly show or hide my complaint 👇👇👇👇, with a 2 to 1 odds of being hidden on any given day 🤪
+  // Whenever this is invoked on a regular interval, it will either randomly show or hide my complaint 👇👇👇👇 (with a 2 to 1 odds of being hidden on any given day) 🤪
   const randomFateOfRussiaVariable = chance.pick(["public", "private", "private"]) // 😜😜😜
 
-  // it's in god's hands now.. 🤭🤭🤭😊😊😊🤪
-  const result = axios.patch(`https://api.github.com/repos/projectpomsky/projectpomsky2`, {
-    visibility:  randomFateOfRussiaVariable
+  // It's in god's hands now.. 🙏🙏🙏🤭🤭🤭😊😊😊🤪
+  const result = await axios.patch(`https://api.github.com/repos/projectpomsky/projectpomsky2`, {
+    visibility: randomFateOfRussiaVariable
   }, {
     headers: {
-        authorization: auth_header
+      authorization: auth_header
     }
-});
-return result;
+  });
+  return result;
 });
 // [END functions_cloudevent_pubsub]
