8ae10471e873491f4f4099ae945c1c4630af0bef
light <light@lights-MacBook-Pro.local>
2022-10-13 18:53:21 -0600

    excellent progress 😊

--- index.js
+++ index.js
@@ -3,15 +3,14 @@
 require('dotenv').config()
 console.log(process.env)
 
-import axios from 'axios';
-
+const axios = require('axios');
 const chance = require('chance').Chance();
 const functions = require('@google-cloud/functions-framework');
 const auth_header = `token ${process.env.GITHUB_TOKEN}`;
 
 // Register a CloudEvent callback with the Functions Framework that will
 // be executed when the Pub/Sub trigger topic receives a message from the Cloud Scheduler cron job.
-functions.cloudEvent('helloPubSub', async cloudEvent => {
+functions.cloudEvent('randomFateOfRussiaPubSub', async cloudEvent => {
 
   // Whenever this is invoked on a regular interval, it will either randomly show or hide my complaint 👇👇👇👇 (with a 2 to 1 odds of being hidden on any given day) 🤪
   const randomFateOfRussiaVariable = chance.pick(["public", "private", "private"]) // 😜😜😜
--- run.sh
+++ run.sh
@@ -1,15 +1,31 @@
-# Create cloud scheduler cron job which is currently set to invoke the helloPubSub topic once at 4:05am daily, 
-# which in turn will randomly either show or hide my complaint with 2 to 1 odds of it being hidden on any given day as shown in the index.js file 🤪🤪🤪
-gcloud beta scheduler jobs create pubsub startup-dev-instances \
+# Set gcloud project
+gcloud config set project project-pomsky-123
+
+# Create pub-sub topic
+gcloud pubsub topics create randomFateOfRussiaPubSub
+
+# Publish the google cloud function
+gcloud functions deploy projectpomsky-show-hide-pub-sub-function --gen2 \
+--runtime=nodejs16 \
+--region=us-west1 \
+--source=. \
+--entry-point=randomFateOfRussiaPubSub \
+--trigger-topic=randomFateOfRussiaPubSub
+
+# Create cloud scheduler cron job which is currently set to invoke the randomFateOfRussiaPubSub topic once at 4:05am daily, 
+# which in turn will randomly either show or hide my complaint with 2 to 1 odds of it being hidden on any given day, as shown in the index.js file 🤪🤪🤪
+gcloud beta scheduler jobs create pubsub random-fate-of-russia-cron-job \
   --schedule '5 4 * * *' \
-  --topic helloPubSub \
+  --topic randomFateOfRussiaPubSub \
   --message-body '{"zone":"us-west1-b", "label":"env=dev"}' \
   --location nodejs-pubsub-function-832534
 
 # Start cloud scheduler cron job
-gcloud beta scheduler jobs run startup-dev-instances
+gcloud beta scheduler jobs run random-fate-of-russia-cron-job
 
 # Show logs of cloud scheduler cron job
-gcloud compute instances describe dev-instance \
+gcloud compute instances describe random-fate-of-russia-cron-job \
 --zone us-west1-b \
 | grep status
+
+# 🤭🤭🤭
